dc7b85da2cef0f750ea95017de2f5835e40ba66a
Elizabeth Kipp <liz@elizabethkipp.com>
2015-05-26 14:26:37 -0500

    tuesday

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 	$('form').submit(function(e) {
 		e.preventDefault();
+
+		if ($('.iteminput').val() === '') {
+			console.log('empty item');
+
+		} else { 
+			addItem()
+
+		}
+
 	});	
+
 	function addItem() {
 		var newItem = $('.iteminput').val();
+		var newItemNode = '<li><span class="item">' + newItem + '</span><img class="trash" src="images/trash.png"></li>';
+		$('ul').append(newItemNode);
 	}
-	$('typekit').load();
+	$('ul').on('click', '.trash', function() {
+		console.log('click trash');
+	})
+
+
+	//$('typekit').load() 	
 });
